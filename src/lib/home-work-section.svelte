da7cf8305e6793a95aa8895ebe1fd5072b06e249
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-04-22 14:10:20 -0400

    Get playwright test passing.

--- src/lib/home-work-section.svelte
+++ src/lib/home-work-section.svelte
@@ -2,4 +2,8 @@
 	import HomeSectionContainer from './home-section-container.svelte';
 </script>
 
-<HomeSectionContainer className="bgColorB" name="work" />
+<HomeSectionContainer className="bgColorB" name="work">
+	<p>your mortgage online</p>
+	<p>loan vista</p>
+	<p>chart templates</p>
+</HomeSectionContainer>
