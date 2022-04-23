19cd99ccc35218e9706616e2420db31ede14d99d
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-04-22 22:53:43 -0400

    Implement back to top button.

--- src/lib/home-contact-section.svelte
+++ src/lib/home-contact-section.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 	import HomeSectionContainer from './home-section-container.svelte';
+
+	let scrollY = 0;
+
+	const handleBackToTop = () => {
+		scrollY = 0;
+	};
 </script>
 
-<HomeSectionContainer className="bgColorA" name="contact" />
+<svelte:window bind:scrollY />
+<HomeSectionContainer className="bgColorA" name="contact">
+	<button on:click={handleBackToTop}>back to top</button>
+</HomeSectionContainer>
