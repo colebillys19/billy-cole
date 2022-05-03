323491eec348a2439918b455fc9d809e79735ed5
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-05-03 14:07:28 -0400

    Show actual error message on 404 page. Add dark mode to 404 page.

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,6 +1,33 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = ({ error }) => ({
+		props: { error }
+	});
+</script>
+
 <script lang="ts">
 	import NavMenu from '$lib/blog-nav-menu.svelte';
+	import store from '../store';
+
+	const { isDarkMode } = store;
+
+	export let error = { message: '' };
 </script>
 
 <NavMenu isErrorNav />
-<main>404</main>
+<main class:isDarkMode={$isDarkMode}>
+	<p>{error.message}</p>
+</main>
+
+<style lang="scss">
+	// dark
+
+	.isDarkMode {
+		background-color: $palette-g;
+	}
+
+	.isDarkMode p {
+		color: $palette-a;
+		min-height: 100vh;
+	}
+</style>
