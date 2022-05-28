f897b337b2f5c71745d855ee41eb2098adf7201f
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2022-05-28 14:14:44 -0400

    Bring in new portfolio images.

--- src/lib/home-work-section.svelte
+++ src/lib/home-work-section.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import store from '../store';
-	import loanhengeImg from '../images/loanhenge.jpg';
+	import loanhengeImgA from '../images/loanhenge-a.jpg';
+	import loanhengeImgB from '../images/loanhenge-b.jpg';
 	import ss3MobileImgA from '../images/ss3-mobile-a.jpg';
 	import ss3MobileImgB from '../images/ss3-mobile-b.jpg';
 	import ss3MobileImgC from '../images/ss3-mobile-c.jpg';
-	import victoryImg from '../images/victory.jpg';
+	import victoryImgA from '../images/victory-a.jpg';
+	import victoryImgB from '../images/victory-b.jpg';
 	import HomeSectionContainer from './home-section-container.svelte';
 
 	const { isDarkMode, workOffset } = store;
@@ -26,14 +28,16 @@
 			excepturi itaque.
 		</p>
 		<h3>Loanhenge</h3>
-		<img class="loanhengeImg" src={loanhengeImg} alt="loanhenge" />
+		<img class="desktopImg" src={loanhengeImgA} alt="loanhenge" />
+		<img class="mobileImg" src={loanhengeImgB} alt="loanhenge" />
 		<p>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero ipsam, porro placeat
 			eaque tempora id labore cumque velit tenetur hic nam inventore quia quod sit non voluptatum
 			excepturi itaque.
 		</p>
 		<h3>Victory Templates</h3>
-		<img class="victoryImg" src={victoryImg} alt="victory" />
+		<img class="desktopImg" src={victoryImgA} alt="victory" />
+		<img class="mobileImg" src={victoryImgB} alt="victory" />
 		<p>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero ipsam, porro placeat
 			eaque tempora id labore cumque velit tenetur hic nam inventore quia quod sit non voluptatum
@@ -47,9 +51,13 @@
 		display: inline-block;
 	}
 
-	.loanhengeImg,
-	.victoryImg {
+	.desktopImg {
+		display: none;
 		width: 900px;
+	}
+
+	.mobileImg {
+		width: 240px;
 	}
 
 	.ss3MobileImg {
@@ -60,5 +68,15 @@
 
 	.isDarkMode p {
 		color: $palette-a;
+	}
+
+	@media (min-width: 768px) {
+		.desktopImg {
+			display: inline-block;
+		}
+
+		.mobileImg {
+			display: none;
+		}
 	}
 </style>
