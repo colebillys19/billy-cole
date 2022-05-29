488195bcc16eeb5ab16abc0d7253bfe024ef75dd
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2022-05-29 16:38:56 -0400

    Add portfolio descriptions and links.

--- src/lib/home-work-section.svelte
+++ src/lib/home-work-section.svelte
@@ -9,7 +9,7 @@
 	import victoryImgB from '../images/victory-b.jpg';
 	import HomeSectionContainer from './home-section-container.svelte';
 
-	const { isDarkMode, workOffset } = store;
+	const { isDarkMode, isMobile, workOffset } = store;
 
 	let innerWidth = 0;
 
@@ -32,11 +32,21 @@
 		{:else}
 			<ImgCarousel />
 		{/if}
-		<p class="hasPaddingBottom">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero ipsam, porro placeat
-			eaque tempora id labore cumque velit tenetur hic nam inventore quia quod sit non voluptatum
-			excepturi itaque.
+		<p>
+			Your Mortgage Online is a widely used iOS/Android application that gives homeowners the
+			ability to easily manage their mortgages. Users can make payments <b>|</b> they can manage
+			escrow accounts, taxes, and insurance <b>|</b> they can contact customer support <b>|</b> and more.
+			I was an integral part of the team that built the app from the ground up.
 		</p>
+		<p class="tools"><b>Tools used:</b></p>
+		<ul class="hasPaddingBottom tools">
+			<li>React Native</li>
+			<li>Redux</li>
+			<li>Redux Saga, Immer, Reselect</li>
+			<li>Node</li>
+			<li>Koa</li>
+			<li>Styled Components</li>
+		</ul>
 		<h3>Loanhenge</h3>
 		<img
 			alt="loanhenge"
@@ -50,11 +60,33 @@
 			src={loanhengeImgB}
 			style={`border-color: ${$isDarkMode ? '#e68a6e' : '#5d5c7b'};`}
 		/>
-		<p class="hasPaddingBottom">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero ipsam, porro placeat
-			eaque tempora id labore cumque velit tenetur hic nam inventore quia quod sit non voluptatum
-			excepturi itaque.
+		<p>
+			Loanhenge is a project I started as a sort of sandbox to experiment with front-end concepts
+			and best-practices while cutting my teeth as a developer. Not long after getting my first job
+			after bootcamp, I was put on a small team tasked with building a React application that'd be
+			used by banks to keep track of bulk loan data. While building that application there were
+			scenarios where I wanted to spend more time fleshing out features and experimenting. I decided
+			to start a project I could work on in my free time that would allow for more freedom. That
+			project became Loanhenge! <span class="opaque">(desktop only)</span>
 		</p>
+		<p class="tools"><b>Tools used:</b></p>
+		<ul class:hasPaddingBottom={$isMobile} class="tools">
+			<li>React</li>
+			<li>Redux</li>
+			<li>Redux Saga, Immer, Reselect</li>
+			<li>Node</li>
+			<li>Express</li>
+			<li>Material UI</li>
+			<li>Styled Components</li>
+			<li>React Testing Library</li>
+		</ul>
+		{#if !$isMobile}
+			<p class="hasPaddingBottom">
+				<a class="bigLink" href="https://loanhenge.herokuapp.com/" target="_blank"
+					>Visit &#x1F440;</a
+				>
+			</p>
+		{/if}
 		<h3>Victory Templates</h3>
 		<img
 			alt="victory"
@@ -69,10 +101,36 @@
 			style={`border-color: ${$isDarkMode ? '#e68a6e' : '#fffeef'};`}
 		/>
 		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi vero ipsam, porro placeat
-			eaque tempora id labore cumque velit tenetur hic nam inventore quia quod sit non voluptatum
-			excepturi itaque.
+			Victory Templates is a project I created as a resource for the devs on my team <span
+				class="opaque">(and for myself!)</span
+			>. We were tasked with recreating a legacy application that implemented various interactive
+			charts. We decided to make use of Formidable's React chart component library
+			<a href="https://formidable.com/open-source/victory/">Victory</a>. I was asked to research the
+			library and put together a repo we could reference as we built out the app. That ask became
+			Victory Templates.
+			<span class="opaque">(desktop only)</span>
 		</p>
+		<p class="tools"><b>Tools used:</b></p>
+		<ul class:hasPaddingBottom={$isMobile} class="tools">
+			<li>React</li>
+			<li>Victory</li>
+			<li>Material UI</li>
+			<li>Styled Components</li>
+		</ul>
+		{#if !$isMobile}
+			<p class="hasPaddingBottom">
+				<a class="bigLink" href="https://loanhenge.herokuapp.com/" target="_blank"
+					>Visit &#x1F440;</a
+				>
+			</p>
+		{/if}
+		<h3>This Site!</h3>
+		<p class="tools"><b>Tools used:</b></p>
+		<ul class="tools">
+			<li>Svelte</li>
+			<li>TypeScript</li>
+			<li>more...</li>
+		</ul>
 	</div>
 </HomeSectionContainer>
 
@@ -84,10 +142,16 @@
 
 	p {
 		margin-top: 24px;
+		text-align: left;
 	}
 
 	img {
 		display: inline-block;
+	}
+
+	ul {
+		margin: 0;
+		text-align: left;
 	}
 
 	.hasPaddingBottom {
@@ -107,11 +171,30 @@
 		width: 252px;
 	}
 
+	.tools {
+		color: $palette-f;
+	}
+
+	.opaque {
+		opacity: 0.6;
+	}
+
+	.bigLink {
+		font-size: 20px;
+		font-weight: 700;
+		text-decoration: none;
+	}
+
 	// dark
 
 	.isDarkMode p,
 	.isDarkMode h3 {
 		color: $palette-a;
+	}
+
+	.isDarkMode a,
+	.isDarkMode .tools {
+		color: $palette-b;
 	}
 
 	@media (min-width: 560px) {
