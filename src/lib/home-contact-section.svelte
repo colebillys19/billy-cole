49861526407fae60dd058c622d42a863a52559a4
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2022-06-03 09:47:42 -0400

    Change scroll behavior. Start on contact section.

--- src/lib/home-contact-section.svelte
+++ src/lib/home-contact-section.svelte
@@ -10,15 +10,54 @@
 </script>
 
 <HomeSectionContainer className="bgColorA" name="contact" {updateOffset}>
-	<div class:isDarkMode={$isDarkMode}>
-		<p>placeholder</p>
+	<div class:isDarkMode={$isDarkMode} class="container">
+		<a href="https://www.google.com/" target="_blank">twitter</a>
+		<a href="https://www.google.com/" target="_blank">linkedin</a>
+		<a href="https://www.google.com/" target="_blank">github</a>
+		<h3>Reach out!</h3>
+		<label for="msg">message:</label>
+		<textarea name="msg" id="msg" cols="30" rows="10" />
+		<div>
+			<button>send</button>
+		</div>
 	</div>
 </HomeSectionContainer>
 
 <style lang="scss">
+	a {
+		display: block;
+		font-size: 20px;
+	}
+
+	a:not(:last-of-type) {
+		margin-bottom: 10px;
+	}
+
+	h3 {
+		margin-top: 24px;
+	}
+
+	label {
+		display: block;
+	}
+
+	textarea {
+		width: 100%;
+	}
+
+	.container {
+		text-align: left;
+	}
+
 	// dark
 
 	.isDarkMode p {
 		color: $palette-a;
+	}
+
+	@media (min-width: 768px) {
+		textarea {
+			width: 600px;
+		}
 	}
 </style>
